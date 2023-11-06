f5aba34aec4c791c3dd58394415d7a4c6f15b063
BYS <trinhbanhat5566@gmail.com>
2023-11-06 22:50:25 +0700

    SIUUUUUUU

--- WebSocket/FE/src/page/chat/Login.tsx
+++ WebSocket/FE/src/page/chat/Login.tsx
@@ -22,7 +22,7 @@ const Login: React.FC<PropLogin> = ({ login }) => {
       <div className={style.login_avar}>
         <h2 className="font-semibold text-[2em]">Chat App</h2>
       </div>
-      <div className={`${style.box} `}>
+      <div className={`${style.box} mt-[-320px]`}>
         <form>
           <h1 className="font-semibold text-[2em]">User</h1>
           <div className={style.inputBox}>
