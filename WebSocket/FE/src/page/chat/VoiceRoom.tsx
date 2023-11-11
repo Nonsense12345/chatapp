a0c55b82107fe838e5a67b2c9031e7f073f1e694
Nonsense12345 <NonsenseZ@github.com>
2023-11-12 01:09:45 +0700

    Add voice Room

--- WebSocket/FE/src/page/chat/VoiceRoom.tsx
+++ WebSocket/FE/src/page/chat/VoiceRoom.tsx
@@ -1,12 +1,24 @@
-// import { useState } from "react";
+import { useState } from "react";
 interface VoiceRoomProps {
   UserName: string;
   Photo: string;
 }
-
+interface User {
+  username: string;
+  photo: string;
+}
 const VoiceRoom = ({ UserName, Photo }: VoiceRoomProps) => {
+  const [open, setOpen] = useState(false);
+
+  const togglePopup = () => {
+    setOpen(!open);
+  };
+  const closePopup = () => {
+    setOpen(false);
+  };
   // const [VoiceWebSocket, setVoiceWebSocket] = useState<WebSocket | null>(null);
-  const readBlobAsBase64 = (blob) =>
+  const [user, SetUser] = useState<User[]>([]);
+  const readBlobAsBase64 = (blob: Blob) =>
     new Promise((resolve, reject) => {
       const reader = new FileReader();
       reader.onloadend = () => resolve(reader.result);
@@ -24,6 +36,18 @@ const VoiceRoom = ({ UserName, Photo }: VoiceRoomProps) => {
     ws.onmessage = function (event) {
       try {
         const data = JSON.parse(event.data);
+
+        if (user?.every((item) => item.username != data.username)) {
+          console.log(data.username, data.photo);
+          SetUser((prevUser) => [
+            ...(prevUser || []),
+            {
+              username: data.username,
+              photo: data.photo,
+            },
+          ]);
+        }
+
         const audioBlob = new Blob(
           [
             new Uint8Array(
@@ -80,7 +104,7 @@ const VoiceRoom = ({ UserName, Photo }: VoiceRoomProps) => {
               audioblob: base64audio,
             })
           );
-          console.log(base64audio);
+          //  console.log(base64audio);
         } catch (error) {
           console.error("Error reading blob as base64:", error);
         }
@@ -102,10 +126,54 @@ const VoiceRoom = ({ UserName, Photo }: VoiceRoomProps) => {
       <button
         onClick={() => {
           connectWebSocket();
+          togglePopup();
         }}
       >
         Join Voice Chat Room
       </button>
+      {open && (
+        <div
+          className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full z-50"
+          id="popupContainer"
+        >
+          <div className="relative top-20 mx-auto p-5 border  shadow-lg rounded-md bg-white">
+            <div className="flex justify-between items-center">
+              <h3 className="text-lg font-medium">Voice Room</h3>
+              <button className="text-black close-popup">&times;</button>
+            </div>
+
+            <div className="mt-2">
+              <p className="text-gray-700">User</p>
+              <ul>
+                {user?.map((item) => (
+                  <div>
+                    <li key={item.username} className="flex items-center mt-2">
+                      <img
+                        src={item.photo}
+                        alt="User"
+                        className="w-8 h-8 rounded-full mr-2"
+                      />
+                      <span>{item.username}</span>
+                    </li>
+                  </div>
+                ))}
+              </ul>
+            </div>
+
+            <div className="flex justify-end mt-4">
+              <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">
+                HeheBOy
+              </button>
+              <button
+                onClick={closePopup}
+                className="px-4 py-2 ml-2 bg-gray-300 text-black rounded hover:bg-gray-500 close-popup"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
